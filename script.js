17d8d75fa9a846279cba7910fe97f34910806d12
Kushal Thapa Magar <107807814+thapakushal@users.noreply.github.com>
2026-03-07 14:08:49 +0545

    Updated typing strings in script.js to reflect new roles

--- script.js
+++ script.js
@@ -1,54 +1,32 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        // sticky navbar on scroll script
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-
-        // scroll-up button show/hide script
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-
-    // slide-up script
-    $('.scroll-up-btn').click(function () {
-        $('html').animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function () {
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
+    // Typing animation
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer", "Backend Developer", "WordPress Developer", "UI/UX Designer", "Creative Designer"],
+        strings: [
+            "Full Stack Developer",
+            "Backend Developer",
+            "WordPress Developer",
+            "UI/UX Designer",
+            "Creative Designer"
+        ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed2 = new Typed(".typing-2", {
-        strings: ["Full Stack Developer", "Backend Developer", "WordPress Developer", "UI/UX Designer", "Creative Designer"],
+        strings: [
+            "Full Stack Developer",
+            "Backend Developer",
+            "WordPress Developer",
+            "UI/UX Designer",
+            "Creative Designer"
+        ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    // owl carousel script
+    // Owl Carousel
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
@@ -70,34 +48,46 @@ $(document).ready(function () {
             }
         }
     });
-});
 
-document.addEventListener('visibilitychange',
-    function () {
-        if (document.visibilityState === "visible") {
-            document.title = "";
-        }
-        else {
-            document.title = "";
+    // Sticky navbar
+    $(window).scroll(function () {
+        if (this.scrollTop() > 20) {
+            $('.navbar').addClass('sticky');
+        } else {
+            $('.navbar').removeClass('sticky');
         }
     });
 
-let loadMoreBtn = document.querySelector('#visit-more');
-let currentItem = 3;
+    // Scroll up button
+    $(window).scroll(function () {
+        if (this.scrollTop() > 500) {
+            $('.scroll-up-btn').addClass('show');
+        } else {
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
 
-loadMoreBtn.onclick = () => {
-    let boxes = [...document.querySelectorAll('.container .box-container .box')];
-    for (var i = currentItem; i < currentItem + 3; i++) {
-        boxes[i].style.display = 'inline-block';
-    }
-    currentItem += 3;
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 }, 'slow');
+    });
 
-    if (currentItem >= boxes.length) {
-        loadMoreBtn.style.display = 'none';
-    }
-}
+    // Toggle navbar
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    });
 
-// resume
-function redirectLinkedIn() {
-    window.location.href = "https://drive.google.com/file/d/1H5yKoqBs1JMB1ItjdT_dUyUehRufH9Io/view?usp=share_link";
-  }
+    // Active menu link on scroll
+    $('section').each(function () {
+        var sectionTop = $(this).offset().top - 70;
+        var sectionBottom = sectionTop + $(this).outerHeight();
+        var sectionId = $(this).attr('id');
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() >= sectionTop && $(this).scrollTop() < sectionBottom) {
+                $('.navbar .menu li a').removeClass('active');
+                $('.navbar .menu li a[href="#' + sectionId + '"]').addClass('active');
+            }
+        });
+    });
+});
